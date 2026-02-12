3ab13e351b8a6836d2df3bdb8dd67db623f2e8a8
Matteo <matteo.marcole3@etu.univ-lorraine.fr>
2026-02-12 14:33:54 +0100

    modele Reservation

--- src/models/Reservation.php
+++ src/models/Reservation.php
@@ -6,4 +6,13 @@ class Reservation extends Model {
     protected $table = 'reservation';
     protected $primaryKey = 'numres';
     public $timestamps = false;
+
+    public function cabine() {
+        return $this->belongsTo(Cabine::class, 'numcab');
+    }
+
+    public function service() {
+        return $this->BelongsToMany(Service::class,'commande','numres','numserv')
+            ->withPivot('nbrinterventions');
+    }
 }
