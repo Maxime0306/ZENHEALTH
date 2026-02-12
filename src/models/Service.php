367d5fb036daa3f80be67775b03e54d6f0f6e794
Matteo <matteo.marcole3@etu.univ-lorraine.fr>
2026-02-12 14:46:15 +0100

    modele service

--- src/models/Service.php
+++ src/models/Service.php
@@ -6,4 +6,9 @@ class Service extends Model {
     protected $table = 'service';
     protected $primaryKey = 'numserv';
     public $timestamps = false;
+
+    public function reservations() {
+        return $this->BelongsToMany(Reservation::class, 'commande', 'numres', 'numserv')
+            ->withPivot('nbrinterventions');
+    }
 }
